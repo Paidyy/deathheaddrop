5d7c2c0e5cb8e3f8f92b9da7eae61e63547e66e2
Paidyy <wieszielonagora2@gmail.com>
2021-08-17 11:21:52 +0200

    Updated

--- src/com/github/Paidyy/deathheaddrop/Main.java
+++ src/com/github/Paidyy/deathheaddrop/Main.java
@@ -1,6 +1,7 @@
 package com.github.Paidyy.deathheaddrop;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import org.bukkit.ChatColor;
@@ -16,9 +17,14 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import me.clip.placeholderapi.PlaceholderAPI;
+
 public class Main extends JavaPlugin implements Listener {
 	@Override
 	public void onEnable() {
+		if (this.getServer().getPluginManager().getPlugin("PlaceholderAPI") == null) {
+			getLogger().info("Could not find PlaceholderAPI, Placeholders from PlaceholderAPI wouldn't work!");
+		}
 		this.getServer().getPluginManager().registerEvents(this, this);
 		saveDefaultConfig();
 		getConfig().options().copyDefaults(true);
@@ -41,17 +47,45 @@ public class Main extends JavaPlugin implements Listener {
                         ItemStack skull = new ItemStack(Material.PLAYER_HEAD, 1);
                         SkullMeta meta = (SkullMeta) skull.getItemMeta();
                         
+                        int day = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+                        int month = Calendar.getInstance().get(Calendar.MONTH);
+                        int year = Calendar.getInstance().get(Calendar.YEAR);
+                        int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+                        int minute = Calendar.getInstance().get(Calendar.MINUTE);
+                        int second = Calendar.getInstance().get(Calendar.SECOND);
+                        
                         String display_name = getConfig().getString("head_display_name");
                         display_name = display_name.replace("%player%", player.getPlayer().getName());
                         display_name = display_name.replace("%killer%", player.getKiller().getName());
+                        display_name = display_name.replace("%day%", Integer.toString(day));
+                        display_name = display_name.replace("%month%", Integer.toString(month));
+                        display_name = display_name.replace("%year%", Integer.toString(year));
+                        display_name = display_name.replace("%hour%", Integer.toString(hour));
+                        display_name = display_name.replace("%minute%", Integer.toString(minute));
+                        display_name = display_name.replace("%second%", Integer.toString(second));
+                        if (this.getServer().getPluginManager().getPlugin("PlaceholderAPI") != null) {
+                        	display_name = PlaceholderAPI.setPlaceholders(player.getPlayer(), display_name);
+                        }
                         meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', display_name));
                         
-                        String lore = getConfig().getString("head_lore");
-                        lore = lore.replace("%player%", player.getPlayer().getName());
-                        lore = lore.replace("%killer%", player.getKiller().getName());
-                		List<String> lore_2 = new ArrayList<String>();
-                		lore_2.add(ChatColor.translateAlternateColorCodes('&', lore));
-                		meta.setLore(lore_2);
+                        
+                        List<String> lore_2 = new ArrayList<String>();
+                    	for (String lore : getConfig().getStringList("head_lore")) {
+                            lore = lore.replace("%player%", player.getPlayer().getName());
+                            lore = lore.replace("%killer%", player.getKiller().getName());
+                            lore = lore.replace("%day%", Integer.toString(day));
+                            lore = lore.replace("%month%", Integer.toString(month));
+                            lore = lore.replace("%year%", Integer.toString(year));
+                            lore = lore.replace("%hour%", Integer.toString(hour));
+                            lore = lore.replace("%minute%", Integer.toString(minute));
+                            lore = lore.replace("%second%", Integer.toString(second));
+                            if (this.getServer().getPluginManager().getPlugin("PlaceholderAPI") != null) {
+                            	lore = PlaceholderAPI.setPlaceholders(player.getPlayer(), lore);
+                            }
+                    		lore_2.add(ChatColor.translateAlternateColorCodes('&', lore));
+                        }
+                    	meta.setLore(lore_2);
+                    	
                         
                         meta.setOwningPlayer(player.getPlayer());
                         skull.setItemMeta(meta);
